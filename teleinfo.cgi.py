6d5eccbeb9d5dc0161e69ac103db091b629ec9e9
frolland04 <frederic.rolland@free.fr>
2018-12-01 22:08:49 +0100

    ajout uname à la cgi

--- teleinfo.cgi.py
+++ teleinfo.cgi.py
@@ -12,14 +12,14 @@ import sys
 
 def readTableRows(name, nb, order='', reverse=False):
     
-    optOrderedBy = ""
+    optOrderedBy = ''
 
     if order != '':
-        optOrderedBy = " order by " + order
+        optOrderedBy = ' order by ' + order
         if reverse:
-            optOrderedBy += " desc"
+            optOrderedBy += ' desc'
 
-    sql = "select * from " + name + optOrderedBy + " limit " + str(nb)
+    sql = 'select * from ' + name + optOrderedBy + ' limit ' + str(nb)
 
     try:
         cs.execute(sql)
@@ -28,27 +28,27 @@ def readTableRows(name, nb, order='', reverse=False):
         d = cs.description
 
     except Exception as e:
-        print("Unable to fetch data from table", name, "!<br><br>", e, "<br><br>")
+        print('Unable to fetch data from table', name, '!<br><br>', e, '<br><br>')
         sys.exit(1)
        
     row_idx = 0
     hdr_idx = 0
 
-    print("<table>")
-    print("<th colspan=", len(d), ">Fetching", nb, "data from table", name, "..</th>")
-    print("<tr>")
+    print('<table>')
+    print('<th colspan=', len(d), '>Fetching', nb, 'data from table', name, '..</th>')
+    print('<tr>')
     for hdr_idx in range(len(d)):
-        print("<th>", d[hdr_idx][0], "</th>")
-    print("</tr>")
+        print('<th>', d[hdr_idx][0], '</th>')
+    print('</tr>')
         
     for row_idx in range(len(s)):
-        print("<tr>")
+        print('<tr>')
         for hdr_idx in range(len(d)):
-            print("<td>", s[row_idx][hdr_idx], "</td>")
-        print("</tr>")
+            print('<td>', s[row_idx][hdr_idx], '</td>')
+        print('</tr>')
     
-    print("</table>")
-    print("<br>")
+    print('</table>')
+    print('<br>')
     return
     
 
@@ -56,7 +56,7 @@ def readTableRows(name, nb, order='', reverse=False):
 # Lecture d'une ligne de la table
 
 def readTableUniqueRow(name):
-    sql = "select * from " + name
+    sql = 'select * from ' + name
 
     try:
         cs.execute(sql)
@@ -65,23 +65,23 @@ def readTableUniqueRow(name):
         d = cs.description
 
     except Exception as e:
-        print("Unable to fetch data from table", name, "!<br><br>", e, "<br><br>")
+        print('Unable to fetch data from table', name, '!<br><br>', e, '<br><br>')
 
     i = 0
 
-    print("<table>")
-    print("<th colspan=3>Fetching data. Table", name, "..</th>")
+    print('<table>')
+    print('<th colspan=3>Fetching data. Table', name, '..</th>')
 
     while i < len(d):
-        print("<tr>")
-        print("<td>", i, "</td>")
-        print("<td>", d[i][0], "</td>")
-        print("<td>", s[0][i], "</td>")
-        print("</tr>")
+        print('<tr>')
+        print('<td>', i, '</td>')
+        print('<td>', d[i][0], '</td>')
+        print('<td>', s[0][i], '</td>')
+        print('</tr>')
         i = i + 1
     
-    print("</table>")
-    print("<br>")
+    print('</table>')
+    print('<br>')
     return
         
 
@@ -95,31 +95,31 @@ def executeRequestUniqueValue(sql):
         s = cs.fetchall()
 
     except Exception as e:
-        print("Unable to fetch data !", "<br><br>", e)
+        print('Unable to fetch data !', '<br><br>', e)
 
     return s[0][0]
 
 
 # -----------------------------------------------------------
-# Programme principal de la "cgi"
+# Programme principal de la 'cgi'
 
-print("Content-type: text/html; charset=UTF-8\n\n")
-print("<!DOCTYPE html>")
-print('<html lang="eng">')
+print('Content-type: text/html; charset=UTF-8\n\n')
+print('<!DOCTYPE html>')
+print("<html lang='eng'>")
 
-print("<head>")
-print('<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>')
-print("<title>TeleInformation ERDF</title>")
-print("</head>")
+print('<head>')
+print("<meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>")
+print('<title>TeleInformation ERDF</title>')
+print('</head>')
 
-print("<body>")
-print("<style>table, th, td { border: 2px solid black; border-collapse: collapse; padding: 10px; } </style>")
+print('<body>')
+print('<style>table, th, td { border: 2px solid black; border-collapse: collapse; padding: 10px; } </style>')
 
-print("Bienvenue sur la page du résumé de la collecte de données TéléInfo ERDF !<br><br>")
+print('Bienvenue sur la page du resume de la collecte de donnees TeleInfo ERDF !<br><br>')
 
 
 # Lecture de l'identification du système Linux
-p = subprocess.Popen(["/bin/uname", "-snrom"], stdout=subprocess.PIPE)
+p = subprocess.Popen(['/bin/uname', '-snrom'], stdout=subprocess.PIPE)
 output = p.communicate()
 uname = output[0]
 
@@ -137,74 +137,74 @@ with open('/proc/uptime', 'r') as f:
     uptime = datetime.timedelta(seconds=float(secs)).days
 
 # Lecture du nom de la machine sous forme de chaine
-p = subprocess.Popen(["/bin/hostname"], stdout=subprocess.PIPE)
+p = subprocess.Popen(['/bin/hostname'], stdout=subprocess.PIPE)
 output = p.communicate()
 hostname = output[0]
 
 # Lecture de l'adresse IPv4 sous forme de chaine
-p = subprocess.Popen(["/bin/hostname", "-I"], stdout=subprocess.PIPE)
+p = subprocess.Popen(['/bin/hostname', '-I'], stdout=subprocess.PIPE)
 output = p.communicate()
 address = output[0]
 
 # Lecture horloge DS3231 sous forme de chaine
-p = subprocess.Popen(["/sbin/hwclock"], stdout=subprocess.PIPE)
+p = subprocess.Popen(['/sbin/hwclock'], stdout=subprocess.PIPE)
 output = p.communicate()
 hwclock = output[0]
 
 # Lecture horloge système sous forme de chaine
-p = subprocess.Popen(["/bin/date"], stdout=subprocess.PIPE)
+p = subprocess.Popen(['/bin/date'], stdout=subprocess.PIPE)
 output = p.communicate()
 sysclock = output[0]
  
 # Connexion à la BDD
-db = MySQLdb.connect("localhost", "teleinfo", "ti", "D_TELEINFO")
+db = MySQLdb.connect('localhost', 'teleinfo', 'ti', 'D_TELEINFO')
 cs = db.cursor()
 
-print("<table>")
-print("<th colspan=2>Histo buffer</th>")
-print("<tr><td>Name</td><td>", hostname, "</td></tr>")
-print("<tr><td>IP</td><td>", address, "</td></tr>")
-print("<tr><td>Model ID</td><td>", model, "</td></tr>")
-print("<tr><td>SN</td><td>", sn, "</td></tr>")
-print("<tr><td>System ID</td><td>", uname, "</td></tr>")
-print("<tr><td>Uptime</td><td>", uptime, "</td></tr>")
-print("<tr><td>DS3231 clock</td><td>", hwclock, "</td></tr>")
-print("<tr><td>System clock</td><td>", sysclock, "</td></tr>")
-print("<tr><td>Collected samples</td><td>", executeRequestUniqueValue("select count(*) from T_HISTO" ), "</td></tr>")
-print("<tr><td>Table allocated (MB)</td><td>", executeRequestUniqueValue('SELECT round(((data_length + index_length) / 1024 / 1024), 2) FROM information_schema.TABLES WHERE table_schema = "D_TELEINFO" AND table_name = "T_HISTO"' ), "</td></tr>")
-print("<tr><td>Global heap max (MB)</td><td>", executeRequestUniqueValue("select round(((@@max_heap_table_size) / 1024 / 1024), 2)" ), "</td></tr>")
-print("</table>")
-print("<br>")
+print('<table>')
+print('<th colspan=2>Histo buffer</th>')
+print('<tr><td>Name</td><td>', hostname, '</td></tr>')
+print('<tr><td>IP</td><td>', address, '</td></tr>')
+print('<tr><td>Model ID</td><td>', model, '</td></tr>')
+print('<tr><td>SN</td><td>', sn, '</td></tr>')
+print('<tr><td>System ID</td><td>', uname, '</td></tr>')
+print('<tr><td>Uptime</td><td>', uptime, '</td></tr>')
+print('<tr><td>DS3231 clock</td><td>', hwclock, '</td></tr>')
+print('<tr><td>System clock</td><td>', sysclock, '</td></tr>')
+print('<tr><td>Collected samples</td><td>', executeRequestUniqueValue('select count(*) from T_HISTO' ), '</td></tr>')
+print('<tr><td>Table allocated (MB)</td><td>', executeRequestUniqueValue('SELECT round(((data_length + index_length) / 1024 / 1024), 2) FROM information_schema.TABLES WHERE table_schema = 'D_TELEINFO' AND table_name = 'T_HISTO'' ), '</td></tr>')
+print('<tr><td>Global heap max (MB)</td><td>', executeRequestUniqueValue('select round(((@@max_heap_table_size) / 1024 / 1024), 2)' ), '</td></tr>')
+print('</table>')
+print('<br>')
     
-readTableUniqueRow("T_COUNTERS")
+readTableUniqueRow('T_COUNTERS')
 
-readTableUniqueRow("T_TELEINFO_INST")
-readTableUniqueRow("T_TEMP_INST")
-readTableUniqueRow("T_RH_INST")
-readTableUniqueRow("T_PA_INST")
+readTableUniqueRow('T_TELEINFO_INST')
+readTableUniqueRow('T_TEMP_INST')
+readTableUniqueRow('T_RH_INST')
+readTableUniqueRow('T_PA_INST')
 
-readTableRows("T_HISTO", 5, "TS", True)
+readTableRows('T_HISTO', 5, 'TS', True)
 
-readTableRows("T_TELEINFO_MIN", 5, "TS_DATE", True)
-readTableRows("T_TELEINFO_MAX", 5, "TS_DATE", True)
-readTableRows("V_TELEINFO_MIN_MAX", 5)
+readTableRows('T_TELEINFO_MIN', 5, 'TS_DATE', True)
+readTableRows('T_TELEINFO_MAX', 5, 'TS_DATE', True)
+readTableRows('V_TELEINFO_MIN_MAX', 5)
 
-readTableRows("T_TEMP_MIN", 5, "TS_DATE", True)
-readTableRows("T_TEMP_MAX", 5, "TS_DATE", True)
-readTableRows("V_TEMP_MIN_MAX", 5)
+readTableRows('T_TEMP_MIN', 5, 'TS_DATE', True)
+readTableRows('T_TEMP_MAX', 5, 'TS_DATE', True)
+readTableRows('V_TEMP_MIN_MAX', 5)
 
-readTableRows("T_RH_MIN", 5, "TS_DATE", True)
-readTableRows("T_RH_MAX", 5, "TS_DATE", True)
-readTableRows("V_RH_MIN_MAX", 5)
+readTableRows('T_RH_MIN', 5, 'TS_DATE', True)
+readTableRows('T_RH_MAX', 5, 'TS_DATE', True)
+readTableRows('V_RH_MIN_MAX', 5)
 
-readTableRows("T_PA_MIN", 5, "TS_DATE", True)
-readTableRows("T_PA_MAX", 5, "TS_DATE", True)
-readTableRows("V_PA_MIN_MAX", 5)
+readTableRows('T_PA_MIN', 5, 'TS_DATE', True)
+readTableRows('T_PA_MAX', 5, 'TS_DATE', True)
+readTableRows('V_PA_MIN_MAX', 5)
 
-readTableRows("T_DBG_ENTRIES", 10)
+readTableRows('T_DBG_ENTRIES', 10)
 
 db.close()
 
-print("</body>")
-print("</html>")
+print('</body>')
+print('</html>')
 
