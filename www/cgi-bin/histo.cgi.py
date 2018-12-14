80fbd538b19a3c008bd40b4f67d7f4155d2dc667
frolland04 <frederic.rolland@free.fr>
2018-12-14 20:37:11 +0100

    cgi py en .cgi.py

--- www/cgi-bin/histo.cgi.py
+++ www/cgi-bin/histo.cgi.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
-# -------------------------------------------------------
-# Exemple d'une 'cgi' en Python, 100% faite à la main :-)
-# -------------------------------------------------------
+import sys
+sys.path.insert(0, '/home/pi/teleinfo/www/common')
+
+"""On récupère le code commun à tous les scripts CGI en Python de Télépi"""
+import telepi_cgi
+
 
 if __name__ == "__main__":
+    """
+    Exemple d'une 'cgi' en Python, 100% faite à la main :-)
+    """
     print('Content-type: text/html; charset=UTF-8\n\n')
     print('<!DOCTYPE html>')
     print("<html lang='FR'>")
@@ -17,7 +23,7 @@ if __name__ == "__main__":
     print('</head>')
 
     print('<body>')
-    print('<p>Coucou depuis la CGI Python!</p>')
+    print('<p>Coucou depuis la CGI Python ' + __file__ + '!</p>')
     print('</body>')
     print('</html>')
 
