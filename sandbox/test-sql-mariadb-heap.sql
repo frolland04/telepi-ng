44008aefdaed13cc4cdbec977c22ed7e6ae3439f
Frédéric ROLLAND <frederic.rolland@free.fr>
2021-03-03 20:00:32 +0100

    dossiers annexes déplacés dans d'autres repo

--- sandbox/test-sql-mariadb-heap.sql
+++
@@ -1,4 +0,0 @@
-select round(((@@max_heap_table_size) / 1024 / 1024), 2), "MB";
-
-SELECT round(((data_length + index_length) / 1024 / 1024), 2), "MB" FROM information_schema.TABLES
-WHERE table_schema = 'D_TELEINFO' AND table_name = 'T_HISTO';
